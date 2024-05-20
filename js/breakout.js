7f6a2ffba3105b8303fe83febf6626db78595270
John Jeffery <142925227+j-jeff001@users.noreply.github.com>
2024-05-20 09:15:48 -0700

    Update breakout.js
    18:50

--- js/breakout.js
+++ js/breakout.js
@@ -44,7 +44,9 @@ bricks = []
 for (let i = 0; i < brickRowCount; i++) {
     bricks[i] = []
     for (let j = 0; j < brickColumnCount; j++) {
-        let x = i * (brickInfo.w)
+        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX
+        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY
+        bricks[i][j] = {x, y, ...brickInfo
     }
 }
 
@@ -92,7 +94,7 @@ console.log(bricks)
 function draw() {
     drawPaddle()
     drawBall()
-    drawScore
+    drawScore()
     drawBricks()
 }
 
